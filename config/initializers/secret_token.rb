85c5daf39524b339fec38fb2b75e9e13a5c4a91c
Elliot Nash <Elliot@VentureMedias.com>
2011-01-25 11:33:04 -0500

    first currensee commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lottery::Application.config.secret_token = 'fac777a6805c45830bf99c25b7e7ffc900e3f22cda7cf0ed0443efd92567142ce266430488cddbdb78013b178789b8d6fb3dc8dcc8dbec911376d857cc829baa'
