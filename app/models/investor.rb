4cfc4bde7de55d00a75053757d9f890efee4dddf
venturemedias <elliot@venturemedias.com>
2011-02-08 15:10:18 -0500

    update log and automatically change to no apple after apple wins

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -92,53 +92,100 @@ class Investor < ActiveRecord::Base
     if investor.prize_id == 2
       @apple.prize_count = @apple.prize_count+1
       @apple.save!
+      @day.number = 2
+      @day.save!
     end
     
   end
   
   def set_second_prize(investor)
     
-    @yournum = rand(100)
-    @day = Day.find_by_id(1)
-    
-    if @day.number == 1
-  
-  
-      if @yournum <= 25
-      investor.prize2_id = 2
-      elsif @yournum > 25 && @yournum <=50
-      investor.prize2_id = 1
-      elsif @yournum > 51 && @yournum <=75
-      investor.prize2_id = 3
-      elsif @yournum > 76 && @yournum <=100
-      investor.prize2_id = 4
+     @yournum = rand(100)
+      @day = Day.find_by_id(1)
+      @custom = Custom.find_by_id(1)
+      @apple = Prizecount.find_by_id(1)
+
+      if @day.number == 1
+
+
+        if @yournum == 1
+        investor.prize2_id = 2
+        elsif @yournum > 1 && @yournum <=4
+        investor.prize2_id = 1
+        elsif @yournum > 4 && @yournum <=15
+        investor.prize2_id = 3
+        elsif @yournum > 15 && @yournum <=100
+        investor.prize2_id = 4
+        end
+
+      elsif @day.number == 2
+
+        if @yournum < 0
+        investor.prize2_id = 2
+        elsif @yournum > 0 && @yournum <=3
+        investor.prize2_id = 1
+        elsif @yournum > 3 && @yournum <=15
+        investor.prize2_id = 3
+        elsif @yournum > 15 && @yournum <=100
+        investor.prize2_id = 4
+        end
+
+      elsif @day.number == 3      
+
+        if @yournum <= 5
+        investor.prize2_id = 2
+        elsif @yournum > 5 && @yournum <=8
+        investor.prize2_id = 1
+        elsif @yournum > 8 && @yournum <= 20
+        investor.prize2_id = 3
+        elsif @yournum > 20 && @yournum <=100
+        investor.prize2_id = 4
+        end
+
+      elsif @day.number == 4      
+
+        if @yournum <= 25
+        investor.prize2_id = 2
+        elsif @yournum > 25 && @yournum <=28
+        investor.prize2_id = 1
+        elsif @yournum > 28 && @yournum <= 40
+        investor.prize2_id = 3
+        elsif @yournum > 40 && @yournum <=100
+        investor.prize2_id = 4
+        end
+
+      elsif @day.number == 5      
+
+        if @yournum <= 50
+        investor.prize2_id = 2
+        elsif @yournum > 50 && @yournum <=53
+        investor.prize2_id = 1
+        elsif @yournum > 53 && @yournum <= 65
+        investor.prize2_id = 3
+        elsif @yournum > 65 && @yournum <=100
+        investor.prize2_id = 4
+        end
+
+      elsif @day.number == 6  
+
+        if @yournum <= @custom.apple
+        investor.prize2_id = 2
+        elsif @yournum > @custom.apple && @yournum <= (@custom.bundle+@custom.apple)
+        investor.prize2_id = 1
+        elsif @yournum > (@custom.bundle+@custom.apple) && @yournum <= (@custom.pickone+@custom.bundle+@custom.apple)
+        investor.prize2_id = 3
+        elsif @yournum > (@custom.pickone+@custom.bundle+@custom.apple) && @yournum <=100
+        investor.prize2_id = 4
+        end
+
       end
-  
-    elsif @day.number == 2
-      
-      if @yournum <= 25
-      investor.prize2_id = 2
-      elsif @yournum > 25 && @yournum <=50
-      investor.prize2_id = 1
-      elsif @yournum > 51 && @yournum <=75
-      investor.prize2_id = 3
-      elsif @yournum > 76 && @yournum <=100
-      investor.prize2_id = 4
+
+      if investor.prize2_id == 2
+        @apple.prize_count = @apple.prize_count+1
+        @apple.save!
+        @day.number = 2
+        @day.save!
       end
-      
-    elsif @day.number == 3      
-      
-      if @yournum <= 50
-      investor.prize2_id = 2
-      elsif @yournum > 51 && @yournum <=90
-      investor.prize2_id = 1
-      elsif @yournum > 91 && @yournum <= 95
-      investor.prize2_id = 3
-      elsif @yournum > 96 && @yournum <=100
-      investor.prize2_id = 4
-      end
-      
-    end
     
     investor.entries = 2
     
